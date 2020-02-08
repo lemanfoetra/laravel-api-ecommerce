58355c1fa2587978c1dccd9ff97465e5f387616b
Leman Fotera <lemanfoetra@gmail.com>
2020-02-08 17:06:21 +0700

    Membuat Exception Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +48,26 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        //dd($exception);
+
+
+        // Hanya tampil jika di header mengirim Accept = application/json  
+        if($request->expectsJson()){
+            if($exception instanceof ModelNotFoundException ){
+                return response()->json([
+                    'status'    => 'error',
+                    'message'   => 'produk tidak ada',
+                ], 404);
+            }
+            if($exception instanceof NotFoundHttpException){
+                return response()->json([
+                    'status'    => 'error',
+                    'message'   => 'Incorrect route/link',
+                ], 404);
+            }
+        }
+        
+
         return parent::render($request, $exception);
     }
 }
