5fefbd39b30b88f6e09b69db8d8c67ae55d512e2
Leman Fotera <lemanfoetra@gmail.com>
2020-02-08 08:03:28 +0700

    Installing Laravel Passport Package

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-
-
 Route::apiResource('product', 'ProductController');
-
 Route::group(['prefix' => 'product'], function(){
 	Route::apiResource('{product}/review', 'ReviewController');
+});
+
+
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('details', 'API\UserController@details');
 });
